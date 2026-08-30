1c1772c2b8208200199a648e2112648d270c78d2
CommitWithSandy <hiremathsandesh14@gmail.com>
2026-08-30 16:19:40 +0530

    smallProject

---
+++ smallProjects/changebgColor/script.js
@@ -0,0 +1,28 @@
+const boxs = document.querySelectorAll('.box');
+const body = document.querySelector('body');
+
+boxs.forEach(function(box){
+    console.log(box);
+    box.addEventListener('click', function(e){
+        console.log(e);
+        console.log(e.target);
+        if (e.target.id === 'gray') {
+            body.style.backgroundColor = e.target.id;
+        }
+        if (e.target.id === 'white') {
+            body.style.backgroundColor = e.target.id;
+        }
+        if (e.target.id === 'blue') {
+            body.style.backgroundColor = e.target.id;
+        }
+        if (e.target.id === 'green') {
+            body.style.backgroundColor = e.target.id;
+        }
+        if (e.target.id === 'orange') {
+            body.style.backgroundColor = e.target.id;
+        }
+        if (e.target.id === 'red') {
+            body.style.backgroundColor = e.target.id;
+        }
+    })
+})
